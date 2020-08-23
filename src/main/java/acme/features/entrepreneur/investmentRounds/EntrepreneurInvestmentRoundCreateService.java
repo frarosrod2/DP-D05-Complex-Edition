2c29a28685ea6c2336af079056ee84aa3ee0fe9a
Paco <frarosrod2@alum.us.es>
2020-08-23 19:35:40 +0200

    Minor fix

--- src/main/java/acme/features/entrepreneur/investmentRounds/EntrepreneurInvestmentRoundCreateService.java
+++ src/main/java/acme/features/entrepreneur/investmentRounds/EntrepreneurInvestmentRoundCreateService.java
@@ -7,12 +7,10 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import acme.entities.accountingRecords.AccountingRecord;
 import acme.entities.activities.Activity;
 import acme.entities.applications.Application;
-import acme.entities.customisations.Customisation;
 import acme.entities.forums.Forum;
 import acme.entities.investmentRounds.InvestmentRound;
 import acme.entities.involvedUsers.InvolvedUser;
@@ -115,90 +113,6 @@ public class EntrepreneurInvestmentRoundCreateService implements AbstractCreateS
 			errors.state(request, isPositive, "money", "entrepreneur.investmentRound.error.negative-money");
 			boolean isEUR = salary.getCurrency().equals("EUR") || salary.getCurrency().equals("€");
 			errors.state(request, isEUR, "money", "entrepreneur.investmentRound.error.money-not-EUR");
-		}
-
-		// Check spam
-
-		Customisation customisation = this.repository.findCustomisation();
-
-		String[] spamWords = customisation.getSpamWords().split(", ");
-		Double threshold = customisation.getSpamThreshold();
-		Boolean isSpamTitle = false;
-		Boolean isSpamDescription = false;
-		Boolean isSpamActivityTitle = false;
-		Boolean isSpamTicker = false;
-		Boolean isSpamLink = false;
-
-		int numberWordsTitle;
-		int numberWordsLink;
-		int numberWordsDescription;
-		int numberWordsActivityTitle;
-		int numberSpam;
-		int numberTicker;
-
-		if (!errors.hasErrors("title")) {
-			String title = entity.getTitle().toLowerCase();
-			numberWordsTitle = title.split("\\W+").length;
-			numberSpam = 0;
-			for (String s : spamWords) {
-				numberSpam += StringUtils.countOccurrencesOf(title, s);
-			}
-			isSpamTitle = numberWordsTitle > 0 && numberSpam * 100 / numberWordsTitle >= threshold;
-			errors.state(request, !isSpamTitle, "title", "entrepreneur.investmentRound.error.isSpam");
-		}
-
-		if (!errors.hasErrors("ticker")) {
-			String ticker = entity.getTicker().toLowerCase();
-			numberTicker = ticker.split("\\W+").length;
-			numberSpam = 0;
-			for (String s : spamWords) {
-				numberSpam += StringUtils.countOccurrencesOf(ticker, s);
-			}
-			isSpamTicker = numberTicker > 0 && numberSpam * 100 / numberTicker >= threshold;
-			errors.state(request, !isSpamTicker, "reference", "entrepreneur.investmentRound.error.isSpam");
-		}
-
-		if (!errors.hasErrors("description")) {
-			String description = entity.getDescription().toLowerCase();
-			numberWordsDescription = description.split("\\W+").length;
-			numberSpam = 0;
-			for (String s : spamWords) {
-				numberSpam += StringUtils.countOccurrencesOf(description, s);
-			}
-			isSpamDescription = numberWordsDescription > 0 && numberSpam * 100 / numberWordsDescription >= threshold;
-			errors.state(request, !isSpamDescription, "description", "entrepreneur.investmentRound.error.isSpam");
-		}
-
-		if (!errors.hasErrors("workProgramme")) {
-			Collection<Activity> workProgramme = this.repository.findAllActivitiesByInvestmentRoundId(entity.getId());
-			for (Activity activity : workProgramme) {
-
-				String activityTitle = activity.getTitle().toLowerCase();
-
-				numberWordsActivityTitle = activityTitle.split("\\W+").length;
-				numberSpam = 0;
-				for (String s : spamWords) {
-					numberSpam += StringUtils.countOccurrencesOf(activityTitle, s);
-				}
-
-				isSpamActivityTitle = numberWordsActivityTitle > 0 && numberSpam * 100 / numberWordsActivityTitle >= threshold;
-				errors.state(request, !isSpamActivityTitle, "activity_title_" + activity.getId(), "entrepreneur.investmentRound.error.isSpam");
-
-			}
-
-		}
-
-		String link = entity.getLink().toLowerCase();
-		if (!link.isEmpty()) {
-			if (!errors.hasErrors("link")) {
-				numberWordsLink = link.split("\\W+").length;
-				numberSpam = 0;
-				for (String s : spamWords) {
-					numberSpam += StringUtils.countOccurrencesOf(link, s);
-				}
-				isSpamLink = numberWordsLink > 0 && numberSpam * 100 / numberWordsLink >= threshold;
-				errors.state(request, !isSpamLink, "link", "entrepreneur.investmentRound.error.isSpam");
-			}
 		}
 
 	}
